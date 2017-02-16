f49eb01ad5646f0e5aec8824a74625888eb5a42b
Оля <vitaliyzhernovoy@gmail.com>
2017-02-16 14:48:40 +0200

    project from class work

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import brainsuper.practice4.Car;
+
+/**
+ * Created by andrii on 1/24/17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Car car = new Car(50);
+
+        car.getDriver();
+
+    }
+}
---
+++ src/module1/FirstProgram.java
@@ -0,0 +1,13 @@
+package module1;
+
+/**
+ * Created by andrii on 1/24/17.
+ */
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("first program works");
+
+        System.out.println("done...");
+    }
+}
